af98583cfbc7ffae317a1382dec0b96297c422a3
Chris <chriskirk1806@gmail.com>
2018-08-03 23:38:57 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Donut/>
+  </div>
+</template>
+
+<script>
+import Donut from "./components/Donut";
+
+export default {
+  name: "App",
+  components: {
+    Donut
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
